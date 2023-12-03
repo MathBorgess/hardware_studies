0ae2108d5298d6c0e1107e307a09a6df4af8d9f4
MathBorgess <matheusborgesfig@gmail.com>
2023-12-03 20:00:39 -0300

    done the P1

--- testbench.sv
+++ testbench.sv
@@ -3,7 +3,7 @@
 `include "sys_design.sv"
 module tb_gate;
   reg [11:0] bits;
-  wire [4:0] count;
+  wire [3:0] count;
 
   LUT_12bit_1s uut (
     .bits(bits),
@@ -14,9 +14,16 @@ module tb_gate;
 
     $dumpvars(0, tb_gate);
 
-    for (bits = 0; bits <= 10; bits = bits + 1) begin
-      #2;
-    end
+    // for(bits = 0; bits < 10; bits = bits + 1) begin
+    //   #10;
+    // end
+
+    bits = 12'b111111111111;
+    #10;
+    bits = 12'b010110101101;
+    #10;
+    bits = 12'b100001011100;
+    #10;
 
     // Finish simulation
     #10 $finish;

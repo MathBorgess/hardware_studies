16f3f54af677aed6f037f0777d507c4f5e7bfc1e
˜MathBorgess˜ <matheusborgesfig@gmail.com>
2024-02-03 15:44:49 -0300

    finished P2

--- digital_system_lessons/P2_files/mux_tb.v
+++ digital_system_lessons/P2_files/mux_tb.v
@@ -5,30 +5,37 @@
 module tb_gate;
   reg [2:0] Sel;
   reg [7:0] A;
-  wire F;
+  wire F1, F2;
 
   mux8_1cond uut1 (
     .Sel(Sel),
     .A(A),
-    .F(F)
+    .F(F1)
   );
 
   mux8_1logic uut2 (
     .Sel(Sel),
     .A(A),
-    .F(F)
+    .F(F2)
   );
 
   initial begin
     $dumpfile("gate_simulation.vcd");
 
     $dumpvars(0, tb_gate);
-    for (Sel = 0; Sel < 8; Sel = Sel + 1) begin
-      for (A = 0; A < 256; A = A + 1) begin
+    for (Sel = 0; Sel < 7; Sel = Sel + 1) begin
+      for (A = 0; A < 255; A = A + 1) begin
         #1;
       end
+      A = 255;
+        #1;
     end
-
+    Sel = 7;
+      for (A = 0; A < 255; A = A + 1) begin
+        #1;
+      end
+      A = 255;
+        #1;
     $finish;
   end
 

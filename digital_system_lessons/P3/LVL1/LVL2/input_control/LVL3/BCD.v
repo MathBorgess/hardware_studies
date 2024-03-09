4e4bf5478a4287d1a42bb72610df255a2bfbb04a
˜MathBorgess˜ <matheusborgesfig@gmail.com>
2024-03-09 17:44:49 -0300

    let's test it!

--- digital_system_lessons/P3/LVL1/LVL2/input_control/LVL3/BCD.v
+++ digital_system_lessons/P3/LVL1/LVL2/input_control/LVL3/BCD.v
@@ -2,15 +2,30 @@ module encoder_BCD (
     input [9:0] keypad, wire enablen,
     output reg [3:0] BCD, reg valid_data
 );
-    initial valid_data = 1'b0;
+    initial begin
+        valid_data = 1'b0;
+        BCD = 4'b1111;
+    end
 
     always@(keypad)
         if (!enablen) begin
-            for ( i = 10'b0000000001; i!=10'b0000000000; i<<1) begin
-                if (keypad[9:0] == i) begin
-                    BCD = i;
-                    valid_data = 1'b1
-                end
-            end
+            case (keypad)
+                10'b0000000001: BCD = 4'b0000;
+                10'b0000000010: BCD = 4'b0001;
+                10'b0000000100: BCD = 4'b0010;
+                10'b0000001000: BCD = 4'b0011;
+                10'b0000010000: BCD = 4'b0100;
+                10'b0000100000: BCD = 4'b0101;
+                10'b0001000000: BCD = 4'b0110;
+                10'b0010000000: BCD = 4'b0111;
+                10'b0100000000: BCD = 4'b1000;
+                10'b1000000000: BCD = 4'b1001;
+                default: BCD = 4'b1111;
+            endcase
+            if(BCD != 4'b1111)
+                valid_data = 1'b1;
+            else
+                valid_data = 1'b0;
         end
+
 endmodule

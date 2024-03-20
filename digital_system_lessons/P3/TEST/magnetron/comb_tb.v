2643d2abd02dd101bacd476ca43428bf0dc43de6
gabriel-gma5 <gma5@cin.ufpe.br>
2024-03-20 20:31:24 -0300

    combin_logic_tb

--- digital_system_lessons/P3/TEST/magnetron/comb_tb.v
+++ digital_system_lessons/P3/TEST/magnetron/comb_tb.v
@@ -16,35 +16,34 @@ module mag_comb_logic_tb;
     );
 
     initial begin
-        $dumpfile("tb.vcd")
-        $dumpvars(0, mag_comb_logic_tb)
+        $dumpfile("tb.vcd");
+        $dumpvars(0, mag_comb_logic_tb);
 
-        startn = 1'b1;
-        stopn = 1'b1;
-        clearn = 1'b1;
-        door_closed = 1'b1;
-        timer_done = 1'b1;
+        startn = 1;
+        stopn = 1;
+        clearn = 1;
+        door_closed = 1;
+        timer_done = 0;
 
-        // case 1: Valid condition for set, all inputs are true except startn
-        #10 startn = 1'b0;
-        #20 stopn = 1'b0;
-        #30 timer_done = 1'b0;
+        // case 1: door opened while timer not done and startn=0
+        #10 door_closed = 0;
+            startn = 0;
 
-        // case 2: Valid condition for reset, all inputs are false except startn
-        #40 startn = 1'b1;
-        #50 stopn = 1'b1;
-        #60 clearn = 1'b0;
-        #70 timer_done = 1'b1;
-        #80 door_closed = 1'b0;
+        // case 2: valid conditions for reset
+        #20 startn = 1;
+            stopn = 0;
+            clearn = 0;
+            timer_done = 1;
+            door_closed = 0;
+        
+        // case 3: set condition
+        #30 clearn = 1;
+            startn = 0;
+            stopn = 1;
+            timer_done = 0;
+            door_closed = 1;
 
-        // case 3: Combination of set and reset conditions
-        #90 startn = 1'b0;
-        #100 stopn = 1'b0;
-        #110 timer_done = 1'b1;
-        #120 door_closed = 1'b1;
-        #130 clearn = 1'b0;
-
-        // End simulation
-        #140 $finish;
+        // end simulation
+        #50 $finish;
     end
 endmodule

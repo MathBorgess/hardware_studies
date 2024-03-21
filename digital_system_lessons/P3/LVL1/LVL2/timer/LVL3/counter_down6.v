68622529e2de64ec0daadc937b8fccae8330ab8d
gabriel-gma5 <gma5@cin.ufpe.br>
2024-03-20 21:18:38 -0300

    test case 2 fail mw controller

--- digital_system_lessons/P3/LVL1/LVL2/timer/LVL3/counter_down6.v
+++ digital_system_lessons/P3/LVL1/LVL2/timer/LVL3/counter_down6.v
@@ -9,7 +9,8 @@ module counter_down6(
     output reg rco_L
 );
     reg [3:0] state, next_state;
-    parameter [3:0] S0 = 4'b0000, S1 = 4'b0001, S2 = 4'b0010, S3 = 4'b0011, S4 = 4'b0100, S5 = 4'b0101;
+    parameter [3:0] S0 = 4'b0000, S1 = 4'b0001, S2 = 4'b0010, S3 = 4'b0011, S4 = 4'b0100, S5 = 4'b0101, S6 = 4'b0110,
+                    S7 = 4'b0111, S8 = 4'b1000, S9 = 4'b1001;
 
     initial begin
         state = S0;
@@ -37,6 +38,11 @@ module counter_down6(
             S3: next_state = S2;
             S4: next_state = S3;
             S5: next_state = S4;
+
+            S6: next_state = S5;
+            S7: next_state = S6;
+            S8: next_state = S7;
+            S9: next_state = S8;
         endcase
     end
 

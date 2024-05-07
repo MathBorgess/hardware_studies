c101e1afccf6f937f04666bc3da624ba0606c0e7
˜MathBorgess˜ <matheusborgesfig@gmail.com>
2024-05-07 15:11:23 -0300

    done

--- infra_hardware/L1/q2.asm
+++ infra_hardware/L1/q2.asm
@@ -1,14 +1,18 @@
 .data
 s: .asciiz "not" # string contem resultado previo ("not")
+.align 2
 eq: .asciiz "eq"
+.align 2
 iso: .asciiz "iso"
+.align 2
 esc: .asciiz "esc"
+.align 2
 l1: .word 9
-l2: .word 10
+l2: .word 7
 l3: .word 9
 
 .text 
-lw $s0, s
+la $s0, s
 lw $s1, l1
 lw $s2, l2
 lw $s3, l3
@@ -25,14 +29,16 @@ and	$t1, $t1, $t2	# $t1 = l2<l1+l3 & l1<l2+l3
 and $t0, $t1, $t0   # $t0 = $t1 & l3<l2+l3
 beq	$t0, $zero, END	# s = not
 
-xor $t0, $s1, $s2 # xor bit a bit -> $t0 = 0 se l1 == l2; um numero qualquer caso contrario
-xor $t1, $s1, $s3
-xor $t2, $s2, $s3
+seq $t0, $s1, $s2 # $t0 = 1 se l1 == l2; 0 caso contrario
+seq $t1, $s1, $s3
+seq $t2, $s2, $s3
 
-beq $t0, $t2, EQ # equilatero se:  l1 = l2 ($t0 = 0) e l2 == l3 ($t2 = 0)
+and $t3, $t0, $t1
+and $t3, $t3, $t2
+bne $t3, $zero, EQ # equilatero se: ($t3 = 1) l1 == l2 e l1 == l3 e l2 == l3
 
-or $t3, $t1, $t2 # isosceles se: l1 == l2 ou l1 == l3 ou l2 == l3 
-or $t3, $t3, $t0
+or $t3, $t0, $t1 # isosceles se: ($t3 = 1) l1 == l2 ou l1 == l3 ou l2 == l3 
+or $t3, $t3, $t2
 bne $t3, $zero, ISO
 
 lw $s0, esc # escaleno
@@ -48,5 +54,8 @@ ISO:
     sw $s0, s
     j END
 END:
+    li $v0, 4
+    la $a0, s
+    syscall
     li	$v0, 10		
     syscall

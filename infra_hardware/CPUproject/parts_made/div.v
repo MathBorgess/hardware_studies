2b6270d3104c9e609510c3639e0d2c184f8ffc72
gabriel-gma5 <gma5@cin.ufpe.br>
2024-06-23 14:54:07 -0300

    div finish, mux HILO

--- infra_hardware/CPUproject/parts_made/div.v
+++ infra_hardware/CPUproject/parts_made/div.v
@@ -10,86 +10,62 @@ module div (
 );
 
 /*
-- DivInit = 1 -> Load srcs
+- DivInit = 1 -> Load srcs and set divRun=1
 - DivInit = 0 -> Do 1 operation; 32 cycles to finish 
-    --  while it's not a DivOp. state , may have dumps in it. 
-        Has no effect on HI/LO since Ctrl_Unit needs to allow load in this registers
+    -- On cycle 32 set divRun=0
 *Steps:
     1. quotient and remainder starts with zero; currDigit (of numerator) starts with 31
     2. shift remainder left and fill with the currDigit, then compare it with denominator
     3.  if denominator>remainder: shift left with 0 as the new LSB of the quotient 
         else: subtract to get new remainder and shift left with 1 as the new LSB of the quotient
-    4. do currDigit-=1
-    5. repeat 2->4 31 more times
-
-    shift divisor right and compare it with current dividend 
-    if divisor is larger, shift 0 as the next bit of the quotient 
-    if divisor is smaller, subtract to get new dividend and shift 1 as the next bit of the quotient
-
+    4.  if currDigit>0: do currDigit-=1 then goto 2.
+        else: divRun = 0
 */	
-    reg divRun, fim, stop;
+    reg divRun = 0;
     reg [31:0] numerator, quotient = 32'b0;
     reg [31:0] denominator, remainder = 32'b0;
-    reg [5:0] currDigit = 5'd31;
-    reg [5:0] cycleCount;
+    reg [5:0] cycleCount, currDigit;
 
     assign hi = remainder;
 	assign lo = quotient;
 
-    assign divStop = stop;
     always @ (posedge clk) begin
-        if(srcB == 32'b0)begin
-            divZero = 1;
-        end
-        // if(divZero)begin
-		//     divZero = 0;
-	    // end
-        // else if(divStop)begin
-		// 	stop = 0;
-		// end
-		else if (reset)begin
+		if (reset) begin
 			remainder=32'b0;
 			denominator=32'b0;
 			numerator=32'b0;
 			quotient=32'b0;
-			currDigit=5'b0;
+			currDigit=5'b11111;
 			cycleCount=5'b0;
-			// fim = 0;
-			// divRun = 0;
-            // stop = 0;
+			divRun = 0;
 		end
-        else if(!divCtrl)begin
-            if(divRun) begin
-                if(currDigit!=6'b111111) begin //-1
-                    remainder = {remainder[30:0],numerator[currDigit]};
-                    if(remainder>=denominator)begin
-                        remainder = remainder - denominator;
-                        quotient = {quotient[30:0],1'b1};  
-                    end
-                    else begin
-                        quotient = {quotient[30:0],1'b0};
-                    end
-                    currDigit <= currDigit-1'b1;
-                    cycleCount <= cycleCount+1'b1;
-                end
-                else begin
-                    divRun=0;
-                    fim=1;
-                    stop = 1;
-                end
+        else if(divCtrl) begin
+            if(srcB == 32'b0) begin
+                divZero = 0;
+            end 
+            else begin
+                numerator = srcA;
+                denominator = srcB;
+                divRun = 1;
+                currDigit = srcA[31];
+                cycleCount = 5'b0;
             end
-        else begin
-            if(fim==0)begin
-                else begin
-                    numerator = value_A;
-                    denominator  = value_B;
-                    currDigit = 5'b11111; //31
-                    divRun = 1;
-                end
+        end
+        else if(!divCtrl & divRun) begin 
+            remainder = {remainder[30:0],numerator[currDigit]};
+            if(denominator>remainder) begin
+                quotient = {quotient[30:0],1'b0};
             end
             else begin
-                fim = 0;
+                remainder = remainder - denominator;
+                quotient = {quotient[30:0],1'b1};  
             end
+            cycleCount <= cycleCount+1'b1;
+            if(cycleCount==5'b11111) begin  // stop div
+                divRun=0;
+            end
+            else begin //-1
+                currDigit <= currDigit-1'b1;
             end
         end
     end

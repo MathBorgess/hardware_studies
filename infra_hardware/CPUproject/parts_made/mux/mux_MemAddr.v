c7c837e539c432a336a58cde64a282ee31c97c98
rmls2 <millerrobert182@gmail.com>
2024-06-23 17:40:22 -0300

    fix: mux_memAddr

--- infra_hardware/CPUproject/parts_made/mux/mux_MemAddr.v
+++ infra_hardware/CPUproject/parts_made/mux/mux_MemAddr.v
@@ -1,5 +1,5 @@
 module memAddr (
-    input wire [3:0] sel,
+    input wire [2:0] sel,
     input wire [31:0] data_0,
     input wire [31:0] data_1,
     input wire [31:0] data_5,
@@ -8,13 +8,13 @@ module memAddr (
 );
     always @(sel) begin
         case (sel)
-            4'd0: data_out = data_0;
-            4'd1: data_out = data_1;
-            4'd2: data_out = 32'd253
-            4'd3: data_out = 32'd254
-            4'd4: data_out = 32'd255
-            4'd5: data_out = data_5;
-            4'd6: data_out = data_6;
+            3'd0: data_out = data_0;
+            3'd1: data_out = data_1;
+            3'd2: data_out = 32'd253
+            3'd3: data_out = 32'd254
+            3'd4: data_out = 32'd255
+            3'd5: data_out = data_5;
+            3'd6: data_out = data_6;
         endcase 
     end 
 
